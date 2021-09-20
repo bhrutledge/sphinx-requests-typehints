87dfa1990f9c358fb960db3e6f96cbe76faac240
Brian Rutledge <brian@bhrutledge.com>
2021-09-19 21:14:57 -0400

    Reformat

--- example_package/__init__.py
+++ example_package/__init__.py
@@ -1,20 +1,22 @@
 import requests
 import http
 
-def function(response: requests.Response, status: http.HTTPStatus) -> requests.Request:
+
+def example_function(
+    response: requests.Response,
+    status: http.HTTPStatus,
+) -> requests.Request:
     """
     Example function using :mod:`requests`.
 
-    It looks like :doc:`autodoc <usage/extensions/autodoc>` is resolving type
-    hints like ``requests.Response`` to their internal definition, e.g.
+    It looks like :doc:`autodoc <usage/extensions/autodoc>` is resolving type hints like
+    ``requests.Response`` to their internal definition, e.g.
     ``requests.models.Response``. This means the :doc:`intersphinx
-    <usage/extensions/intersphinx>` references don't work, resulting in build
-    messages like ``WARNING: py:class reference target not found:
-    requests.models.Response``.
+    <usage/extensions/intersphinx>` references don't work, resulting in build messages
+    like ``WARNING: py:class reference target not found: requests.models.Response``.
 
-    For comparison, this function includes type hints for the Python standard
-    library and explicit references to the public APIs via
-    `interphinx-untangled
+    For comparison, this function includes type hints for the Python standard library
+    and explicit references to the public APIs via `interphinx-untangled
     <https://webknjaz.github.io/intersphinx-untangled/>`_.
 
     :param response: A :class:`requests.Response` instance.
